02c27acec605dd304602eabfbaa06aa7e18be8d1
siva-pm <siva.pm@zohotrainees.com>
2025-01-01 13:40:19 +0530

    modified to multiple process creation

--- intro_process.c
+++ intro_process.c
@@ -3,18 +3,39 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int main() {
-    pid_t pid = fork();
+void task_doer(int task_num)
+{
+    printf("task %d (PID: %d) started, Parent PID: %d\n", task_num, getpid(),getppid());
+    sleep(2+ task_num); // real work should be inserted here
+    printf("task %d (PID: %d) completed\n\n2",task_num,getpid());
+}
 
-    if (pid == 0) {
-        //sleep(2);
-        printf("Child Process: PID = %d\n", getpid());
-    } else if (pid > 0) {
-        printf("Parent Process: PID = %d\n", getpid());
-        wait(NULL);
-        //sleep(2);
-    } else {
+int main() {
+    int count;
+    printf("enter the no. of child process to create: ");
+    scanf("%d",&count);
+    pid_t cpids[count];
+
+    for(int i=0;i<count;i++)
+    {
+        pid_t pid=fork();
+        if (pid == 0) 
+        printf("Child Process: PID = %d\n", getpid()); 
+        else if (pid > 0) 
+        cpids[i]=pid;
+        else {
         perror("fork failed");
+        exit(1);}
     }
+    
+    for(int i=0; i<count;i++)
+    {
+        int status;
+        pid_t pid=wait(&status);
+        if(WIFEXITED(status))
+        printf("pid: %d exited with status %d\n ",pid,WEXITSTATUS(status));
+    }
+
+    printf("all child processes completed. parent exiting. \n");
     return 0;
 }
