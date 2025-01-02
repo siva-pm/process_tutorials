831cf4c3417d1854f860918434c17b2a8fabf35d
siva-pm <siva.pm@zohotrainees.com>
2025-01-02 17:30:26 +0530

    preemptive nature in single process pgm

--- single_process.c
+++ single_process.c
@@ -2,23 +2,21 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-int count=0;
-
 int main() {
+    int n;
     pid_t pid = fork();
+    
+    if(pid==0)
+    n=1;
+    else
+    n=6;
 
-    if (pid == 0) {
-        //sleep(2);
-        printf("Child Process: PID = %d Parent Process: PID = %d\n", getpid(),getppid()); 
-        printf("child executed\n");} 
-    else if (pid > 0) {
-        printf("Parent Process: PID = %d parents parent Process: PID = %d\n", getpid(),getppid());      
-        printf("parent executed\n");} 
-    else {
-        perror("fork failed");}
-    count++;
-    printf("count is: %d\n",count); //the global variable doesnt reflect the change done in child process
-    printf("child pid is: %d\n",wait(NULL)); //wait joins and returns the childs pid when parent is running
-    printf("who did this, %d\n",getpid());
+    for(int i=n;i<n+5;i++)
+    printf(" %d ",i),fflush(stdout);
+
+    if(pid>0)
+    wait(NULL);
+    
+    printf("\n");
     return 0;
 }
