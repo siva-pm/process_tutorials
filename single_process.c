54404c0d9fe09d3403158571dac5f58337804d51
siva-pm <siva.pm@zohotrainees.com>
2025-01-02 19:45:32 +0530

    orphan process simulation with single process

--- single_process.c
+++ single_process.c
@@ -3,20 +3,11 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 int main() {
-    int n;
     pid_t pid = fork();
-    
+
     if(pid==0)
-    n=1;
-    else
-    n=6;
+    sleep(2);    //put child process to sleep for awhile
 
-    for(int i=n;i<n+5;i++)
-    printf(" %d ",i),fflush(stdout);
-
-    if(pid>0)
-    wait(NULL);
-    
-    printf("\n");
+    printf("current id: %d, parent id: %d \n",getpid(),getppid());
     return 0;
 }
